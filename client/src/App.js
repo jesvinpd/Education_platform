b59da23423b050ac8e3dcccf4d954589b74c82c2
LAPTOPP5 <125674717+LAPTOPP5@users.noreply.github.com>
2025-06-27 18:14:43 +0530

    implemented micro frontend architecture based notes section inside the our host notes section

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import DashBoard from "./components/DashBoard";
@@ -6,6 +6,8 @@ import Problem from "./components/dashboard/Problem";
 import codingProblems from "./dummyData";
 import PracticeSection from "./components/dashboard/PracticeSection";
 
+// Lazy load remote components
+const NotesApp = lazy(() => import("notesApp/NotesApp"));
 function App() {
   return (
     <Router>
@@ -13,8 +15,18 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/dashboard" element={<DashBoard />}>
           <Route index element={<div>Dashboard Content</div>} />
-          <Route path="notes" element={<div>Notes Content</div>} />
-          <Route path="practice" element={<PracticeSection problems={codingProblems} />} />
+          <Route
+            path="notes"
+            element={
+              <Suspense fallback={<h1>Loading Notes...</h1>}>
+                <NotesApp />
+              </Suspense>
+            }
+          />
+          <Route
+            path="practice"
+            element={<PracticeSection problems={codingProblems} />}
+          />
         </Route>
         <Route
           path="/problem/:id"
