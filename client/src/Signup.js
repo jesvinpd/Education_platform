bc12ec73a1053c2a77eae155787c2fe26b423721
LAPTOPP5 <125674717+LAPTOPP5@users.noreply.github.com>
2025-08-16 21:23:24 +0530

    axios added to client and signUp

--- client/src/Signup.js
+++ client/src/Signup.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
+import axios from "axios";
 import "./Auth.css"; // Shared CSS
 
 const Signup = () => {
